637a29f0a6b38efdf2f27630cce7c665ab76ffc8
Dao Trong Thinh <thinh.daotrong178@gmail.com>
2019-08-06 21:17:06 +0700

    update

--- js/main.js
+++ js/main.js
@@ -3,10 +3,10 @@ document.addEventListener("DOMContentLoaded", function () {
     var status = ">0";
     var btnScrollToTop = document.querySelector(".scroll-top");
 
-    var resumePosition = document.querySelector('#resume').offsetTop - 50;
-    var productsPosition = document.querySelector('#products').offsetTop - 50;
-    var contactPosition = document.querySelector('#contact').offsetTop - 50;
-
+    var resumePosition = document.querySelector('.resume').offsetTop - 60;
+    var productsPosition = document.querySelector('.products').offsetTop - 60;
+    var contactPosition = document.querySelector('.contact').offsetTop -60;
+    
     window.addEventListener("scroll", function () {
         if (window.pageYOffset > 0) {
             if (status === ">0") {
@@ -23,38 +23,40 @@ document.addEventListener("DOMContentLoaded", function () {
         };
         
         if (window.pageYOffset >= 0 && window.pageYOffset < resumePosition) {
-            for (let i = 0; i < navLink.length; i++) {
-                navLink[i].classList.remove("active");
+            for (let i = 0; i < navItem.length; i++) {
+                navItem[i].classList.remove("active");
             }
-            navLink[0].classList.add("active");
+            navItem[0].classList.add("active");
         }
         if (window.pageYOffset >= resumePosition && window.pageYOffset < productsPosition) {
-            for (let i = 0; i < navLink.length; i++) {
-                navLink[i].classList.remove("active");
+            for (let i = 0; i < navItem.length; i++) {
+                navItem[i].classList.remove("active");
             }
-            navLink[1].classList.add("active");
+            navItem[1].classList.add("active");
         }
         if (window.pageYOffset >= productsPosition && window.pageYOffset < contactPosition) {
-            for (let i = 0; i < navLink.length; i++) {
-                navLink[i].classList.remove("active");
+            for (let i = 0; i < navItem.length; i++) {
+                navItem[i].classList.remove("active");
             }
-            navLink[2].classList.add("active");
+            navItem[2].classList.add("active");
         }
         if (window.pageYOffset >= contactPosition) {
-            for (let i = 0; i < navLink.length; i++) {
-                navLink[i].classList.remove("active");
+            for (let i = 0; i < navItem.length; i++) {
+                navItem[i].classList.remove("active");
             }
-            navLink[3].classList.add("active");
+            navItem[3].classList.add("active");
         }
     });
+
     btnScrollToTop.addEventListener("click", function () {
         document.documentElement.scrollTop = 0;
     });
-    var navLink = document.querySelectorAll(".nav-link");
-    for (let i = 0; i < navLink.length; i++) {
-        navLink[i].addEventListener("click", function () {
-            let activeLink = document.querySelector(".navbar .active");
-            activeLink.classList.remove("active");
+
+    var navItem = document.querySelectorAll(".nav-item");
+    for (let i = 0; i < navItem.length; i++) {
+        navItem[i].addEventListener("click", function () {
+            let activeItem = document.querySelector(".navbar .active");
+            activeItem.classList.remove("active");
             this.classList.add("active");
 
             let sectionID = document.querySelector(".navbar .active").getAttribute("data-target");
@@ -64,5 +66,34 @@ document.addEventListener("DOMContentLoaded", function () {
 
             document.querySelector(".navbar-collapse").classList.remove("show");
         });
+    };
+
+    $('.products-list').isotope({
+        itemSelector: '.products-item',
+        layoutMode: 'fitRows'
+    });
+
+    var filterItem = document.querySelectorAll('.products-filter li');
+    for (let i = 0; i < filterItem.length; i++) {
+        filterItem[i].addEventListener('click', function(){
+            for (let j = 0; j < filterItem.length; j++) {
+                filterItem[j].classList.remove('active');
+            }
+            this.classList.add('active');
+        })
     }
+    $('.products-filter li a').click(function (e) { 
+        e.preventDefault();
+        let item = $(this).data('item');
+        if(item === "all"){
+            $('.products-list').isotope({
+                filter: '*'
+            });
+        }
+        else {
+            $('.products-list').isotope({
+                filter: item
+            });
+        }
+    });
 });
